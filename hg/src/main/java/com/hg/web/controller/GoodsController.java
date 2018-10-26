32970d82514169a9ae524ad2e8bc2f70b31eb9a8
wangxiaochuan <wangxiaochuan01@163.com>
2018-10-26 14:13:53 +0800

    refactor folder structure and pom

--- hg/src/main/java/com/hg/web/controller/GoodsController.java
+++ hg/src/main/java/com/hg/web/controller/GoodsController.java
@@ -14,29 +14,30 @@ import java.util.List;
  * @date 2018/8/22 9:53
  */
 @RestController
+@RequestMapping("/web")
 public class GoodsController {
     @Autowired
     private GoodsService goodsService;
 
-    @RequestMapping(value = "/web/goods/addGoods", method = RequestMethod.POST)
+    @RequestMapping(value = "/goods/addGoods", method = RequestMethod.POST)
     HgResponse<Goods> addGoods(@RequestBody Goods goods) {
         goodsService.save(goods);
         return HgResponse.success(goods);
     }
 
-    @RequestMapping(value = "/web/goods/updateGoods", method = RequestMethod.POST)
+    @RequestMapping(value = "/goods/updateGoods", method = RequestMethod.POST)
     HgResponse<Goods> updateGoods(@RequestBody Goods goods) {
         goodsService.updateById(goods);
         return HgResponse.success(goods);
     }
 
-    @RequestMapping(value = "/web/goods/deleteGoods", method = RequestMethod.POST)
+    @RequestMapping(value = "/goods/deleteGoods", method = RequestMethod.POST)
     HgResponse deleteGoods(@RequestParam int id) {
         goodsService.removeById(id);
         return HgResponse.success();
     }
 
-    @RequestMapping(value = "/web/goods/getGoodsList", method = RequestMethod.GET)
+    @RequestMapping(value = "/goods/getGoodsList", method = RequestMethod.GET)
     HgResponse<List<Goods>> getGoodsList() {
         return HgResponse.success(goodsService.list(null));
     }
