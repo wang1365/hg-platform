a27e8fe6549c7e7561e531e11edac1ca182acb77
wontfalling <rongjun.gao@foxmail.com>
2018-10-30 00:36:15 +0800

    area: add updateArea api

--- hg/src/main/java/com/hg/web/rest/AreaController.java
+++ hg/src/main/java/com/hg/web/rest/AreaController.java
@@ -1,4 +1,4 @@
-package com.hg.web.controller;
+package com.hg.web.rest;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.hg.web.common.HgResponse;
@@ -7,6 +7,8 @@ import com.hg.web.service.AreaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,4 +23,11 @@ public class AreaController {
     public HgResponse<List<Area>> getAreaList() {
         return HgResponse.success(areaService.list(new QueryWrapper<Area>()));
     }
+
+    @PostMapping("/updateArea")
+    public HgResponse<Area> updateArea(@RequestBody Area area) {
+        areaService.updateById(area);
+        return HgResponse.success(area);
+    }
+
 }
